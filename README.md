a03c768d48eed7d5351ab56b194dcbda97ffd611
Daniel Gallagher <daniel@malware.exposed>
2019-05-09 22:41:58 -0400

    Cleaned up unnecessary code
    Removed code that is not needed after the addition of automatic lookup.

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
 # splunk-updater
 Automate downloading the latest Splunk installer
 
-Usage: ./splunk_updater.sh _product_ _version_ _hash_ _arch_ _pkg_
+Usage: ./splunk_updater.sh _product_ _arch_ _pkg_
+
+# Optional Arguments:
 
 * [1] Product -> Values can be: splunk, splunkforwarder
-* [2] Version -> Example: 7.2.6
-* [3] Hash -> Example: c0bf0f679ce9
-* [4] Arch -> Values can be: x86_64 (redhat), amd64 (ubuntu)
-* [5] Pkg -> Values can be: rpm, deb
+* [2] Arch -> Values can be: x86_64 (redhat), amd64 (ubuntu)
+* [3] Pkg -> Values can be: rpm, deb
 
 # Current defaults:
 
 * Product: splunk
-* Version: 7.2.6
-* Hash: c0bf0f679ce9
 * Arch: amd64
 * Pkg: deb
--- splunk_updater.sh
+++ splunk_updater.sh
@@ -5,17 +5,18 @@
 #             -Daniel Gallagher             #
 #-------------------------------------------#
 
-# Initialze variables
-os="linux" # Set static to "linux" for now
-
+# Lookup current Splunk version
 long_version=$(curl -s https://www.splunk.com/en_us/download/splunk-enterprise.html | egrep -o 'splunk-[0-9]+\.[0-9]+\.[0-9]+-[0-9a-f]{8,12}' | head -1)
 version=$(echo $long_version | cut -d - -f 2) # Splunk product version
 hash=$(echo $long_version | cut -d - -f 3) # Hash is specific to version
 
+# Initialize variables with defaults
+os="linux" # Set static to "linux" for now
 product=${1:-splunk} # values can be : splunk , splunkforwarder
 arch=${2:-amd64} # values should be : x86_64 (redhat), amd64 (ubuntu)
 pkg=${3:-deb} # Values should be : rpm, deb
 
+# Generate filename
 filename="${product}-${version}-${hash}-${os}-2.6-${arch}.${pkg}"
 md5File="${filename}.md5"
 
@@ -23,13 +24,11 @@ echo -e "\e[35m[*] Current Version: $long_version \e[0m"
 
 function usage
 {
-  echo -e "\e[96m[+] Usage: $0 <product> <version> <hash> <arch> <pkg> \e[0m"
-  echo -e "\e[96m[+] --- Arguments --- \e[0m"
+  echo -e "\e[96m[+] Usage: $0 <product> <arch> <pkg> \e[0m"
+  echo -e "\e[96m[+] --- Optional Arguments --- \e[0m"
   echo -e "\e[96m[+] Product -> Values can be: splunk, splunkforwarder \e[0m" #option 1
-  echo -e "\e[96m[+] Version -> Example: 7.2.6 \e[0m" #option 2
-  echo -e "\e[96m[+] Hash -> Example: c0bf0f679ce9 \e[0m" #option 3
-  echo -e "\e[96m[+] Arch -> Values can be: x86_64 (redhat), amd64 (ubuntu) \e[0m" #option 4
-  echo -e "\e[96m[+] Pkg -> Values can be: rpm, deb \e[0m" #option 5
+  echo -e "\e[96m[+] Arch -> Values can be: x86_64 (redhat), amd64 (ubuntu) \e[0m" #option 2
+  echo -e "\e[96m[+] Pkg -> Values can be: rpm, deb \e[0m" #option 3
   exit
 }
 
