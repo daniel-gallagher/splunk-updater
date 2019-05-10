1b4972a5b16f9cee2211671a783e5fb04e840b9c
Daniel Gallagher <daniel@malware.exposed>
2019-05-10 01:40:00 -0400

    Added basic .rpm support
    This will allow for the install of rpm files, but requires changing two static variables. Need to implement arguments.

--- splunk_updater.sh
+++ splunk_updater.sh
@@ -8,16 +8,15 @@
 # Lookup current Splunk version
 long_version=$(curl -s https://www.splunk.com/en_us/download/splunk-enterprise.html | egrep -o 'splunk-[0-9]+\.[0-9]+\.[0-9]+-[0-9a-f]{8,12}' | head -1)
 version=$(echo $long_version | cut -d - -f 2)
-hash=$(echo $long_version | cut -d - -f 3)
+build=$(echo $long_version | cut -d - -f 3)
 
 # Static variables (adjust to your environment)
-os="linux" # Linux only for now
 product="splunk" # values can be : splunk , splunkforwarder
 arch="amd64" # values can be : x86_64 (redhat), amd64 (ubuntu)
 pkg="deb" # Values can be : rpm (redhat), deb (ubuntu)
 
 # Generate filename
-filename="${product}-${version}-${hash}-${os}-2.6-${arch}.${pkg}"
+filename="${product}-${version}-${build}-${os}-2.6-${arch}.${pkg}"
 md5File="${filename}.md5"
 
 
@@ -31,13 +30,13 @@ function usage
 function get_update
 {
   echo -e "\e[35m[*] Current Version: $long_version \e[0m"
-  echo -e "\e[90m[+] Downloading: $md5File \e[0m"
+  echo -e "\e[94m[+] Downloading: $md5File \e[0m"
 
-  wget "https://download.splunk.com/products/splunk/releases/${version}/${os}/${md5File}" -q --show-progress
+  wget "https://download.splunk.com/products/splunk/releases/${version}/linux/${md5File}" -q --show-progress
 
-  echo -e "\e[90m[+] Downloading: $filename \e[0m"
+  echo -e "\e[94m[+] Downloading: $filename \e[0m"
 
-  wget "https://download.splunk.com/products/splunk/releases/${version}/${os}/${filename}" -q --show-progress
+  wget "https://download.splunk.com/products/splunk/releases/${version}/linux/${filename}" -q --show-progress
 
   echo -e "\e[94m[+] Checking md5sum of: $filename \e[0m"
 
@@ -54,10 +53,24 @@ function get_update
 
 function install_update
 {
-  echo -e "\e[90m[+] Installing: $filename \e[0m"
+  echo -e "\e[94m[+] Installing: $filename \e[0m"
 
-  sudo dpkg -i $filename
-  sudo su - splunk -c '/opt/splunk/bin/splunk start --accept-license --answer-yes --no-prompt'
+  if [ "$pkg" = "deb" ]; then
+    sudo dpkg -i $filename >>update.log
+  fi
+
+  if [ "$pkg" = "rpm" ]; then
+    sudo rpm -Uvh $filename >>update.log
+  fi
+
+  echo -e "\e[94m[+] Restarting Splunk services \e[0m"
+  sudo su - splunk -c '/opt/splunk/bin/splunk start --accept-license --answer-yes --no-prompt' >>update.log
+
+  echo -e "\e[94m[-] Cleaning up: $filename \e[0m"
+  rm -f ./$filename
+
+  echo -e "\e[94m[-] Cleaning up: $md5File \e[0m"
+  rm -f ./$md5File
 
   echo -e "\e[93m[+] Upgrade complete! \e[0m"
 }
